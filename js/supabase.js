b73825f674ffb441913766f25a46a0121a1040ce
Pedro Honorio <pedrobhonorio@gmail.com>
2026-05-05 21:16:44 -0300

    Adiciona login e recuperação de senha

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://akqdwxnjhhtoktzrppal.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_pm8pSmWeeYEZnSNqc9SHhg_ENPcjlSJ";
+
+const supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
